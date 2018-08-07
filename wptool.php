76211856830c2cef5ae021aea27833f36c825838
John Mclaren <john@johnscs.com>
2018-08-07 03:12:48 -0400

    Updated jquery

--- wptool.php
+++ wptool.php
@@ -246,7 +246,6 @@ if(isset($_POST["command"])){
 
     <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script>
             $( document ).ready(function() {
                 $('.autoform').submit(function(event) {
